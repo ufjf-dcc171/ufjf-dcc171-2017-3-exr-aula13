3f1241010d6de074e723978c770c45849b96bab7
Igor Knop <igorknop@gmail.com>
2017-10-17 21:30:38 -0200

    Cria classe Lancamento

---
+++ src/br/ufjf/dcc171/Lancamento.java
@@ -0,0 +1,51 @@
+package br.ufjf.dcc171;
+
+import java.util.Date;
+
+public class Lancamento {
+    private String descricao;
+    private Date data;
+    private Double valor;
+
+    public Lancamento() {
+        this(null, new Date(), null);
+    }
+
+    public Lancamento(String descricao, Double valor) {
+        this(descricao, new Date(), valor);
+    }
+
+    
+    
+    public Lancamento(String descricao, Date data, Double valor) {
+        this.descricao = descricao;
+        this.data = data;
+        this.valor = valor;
+    }
+
+    
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+    
+}
